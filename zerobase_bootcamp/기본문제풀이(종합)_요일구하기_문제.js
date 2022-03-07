f52406a0b22ead7d9b26e775ed3303b7afe7c897
jinnkimm7 <jinnkimm7@gmail.com>
2022-03-07 22:49:43 +0900

    요일 구하기 문제

---
+++ zerobase_bootcamp/기본문제풀이(종합)_요일구하기_문제.js
@@ -0,0 +1,35 @@
+/* 4. 요일 구하기 */
+
+/* user code */
+function answer(str) {
+  let week = new Array(
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일"
+  );
+  let day;
+
+  const date = new Date(str);
+  const day_num = date.getDay();
+  day = week[day_num];
+
+  return day;
+}
+
+/* main code */
+let input = [
+  // TC: 1
+  "2021-01-27",
+  // TC: 2
+  "2021-02-27",
+  // TC: 3
+  "2021-03-14",
+];
+
+for (let i = 0; i < input.length; i++) {
+  console.log(`#${i + 1} ${answer(input[i])}`);
+}
