5a741ab6b69f057d00f066faf85f328a964835a8
jinnkimm7 <jinnkimm7@gmail.com>
2022-03-23 17:16:10 +0900

    Create Lv1_제일작은수_제거하기.js

---
+++ programmers/Lv1_제일작은수_제거하기.js
@@ -0,0 +1,29 @@
+// function solution(arr) {
+//   let min;
+//   if (arr.length > 1) {
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] > arr[i + 1]) min = arr[i + 1];
+//       else min = arr[i];
+//     }
+//     let idx = arr.indexOf(min);
+
+//     arr.splice(idx, 1);
+//     return arr;
+//   }
+//   else return [-1];
+// }
+
+// console.log(solution([4, 3, 2, 1]));
+// console.log(solution([10]));
+
+
+// 다른 사람 풀이
+const solution = (arr) => {
+  arr.splice(arr.indexOf(Math.min(...arr)), 1);
+  return arr.length ? arr : [-1];
+};
+
+console.log(solution([4, 3, 2, 1]));
+console.log(solution([10]));
+
+
