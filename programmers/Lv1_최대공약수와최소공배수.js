5459cea47dcfaa2807fa9ea80c0aa14eb40bf89b
jinnkimm7 <jinnkimm7@gmail.com>
2022-03-26 18:48:46 +0900

    programmers

--- programmers/Lv1_최대공약수와최소공배수.js
+++ programmers/Lv1_최대공약수와최소공배수.js
@@ -1,4 +1,11 @@
 function solution(n, m) {
   var answer = [];
-  return answer;
+  const greatest = (a, b) => {
+    if (b === 0) return a
+    return greatest(b, a % b)
+  }
+  const least = (a, b) => (a * b) / greatest(a, b)
+  return [greatest(n, m), least(n, m)]
 }
+
+console.log(solution(3, 12));
