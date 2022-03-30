cf9527a7f1f5f8d877980f4b3a70ee8bdbbf403d
jinnkimm7 <jinnkimm7@gmail.com>
2022-03-30 23:11:44 +0900

    Create Lv1_K번째수.js

---
+++ programmers/Lv1/Lv1_K번째수.js
@@ -0,0 +1,14 @@
+function solution(array, commands) {
+  var answer = [];
+
+  for (var i = 0; i < commands.length; i++) {
+    var list = array.slice(commands[i][0] - 1, commands[i]
+    [1]).sort((a, b) => { return a - b });
+
+    answer.push(list[commands[i][2] - 1]);
+  }
+
+  return answer;
+}
+
+console.log(solution([1, 5, 2, 6, 3, 7, 4], [2, 5, 3]));
