01f5b01f816266b100f3644fbf555d09c84632bf
jinnkimm7 <jinnkimm7@gmail.com>
2022-04-07 20:36:57 +0900

    zerobase_test

--- zerobase_bootcamp/zerobase_test/4-5.js
+++ zerobase_bootcamp/zerobase_test/4-5.js
@@ -1,29 +1,23 @@
+/** 
+우리나라의 화폐단위는 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 원으로 이루어져 있습니다.
+거스름돈 금액 A원이 입력 되었을때 해당 거스름돈을 만족하는 최소한의 화폐 개수를 리턴하는 함수를 작성하세요.
+
+[입력]
+거스름돈 금액 A
+
+[출력]
+최소한의 화폐 개수
+*/
+
 function solution(A) {
-  let change = A;
-
-  const fiftyThousand = (change - (change % 50000)) / 50000;
-  change = change - (50000 * fiftyThousand);
-  const tenThousand = (change - (change % 10000)) / 10000;
-  change = change - (50000 * tenThousand);
-  const fiveThousand = (change - (change % 5000)) / 5000;
-  change = change - (500 * fiveThousand);
-  const oneThousand = (change - (change % 1000)) / 1000;
-  change = change - (1000 * oneThousand);
-  const fiveHundred = (change - (change % 500)) / 500;
-  change = change - (500 * fiveHundred);
-  const oneHundred = (change - (change % 100)) / 100;
-  change = change - (100 * oneHundred);
-  const fifty = (change - (change % 50)) / 50;
-  change = change - (50 * fifty);
-  const ten = (change - (change % 10)) / 10
-  change = change - (10 * ten);
-  const five = (change - (change % 5)) / 5
-  change = change - (5 * five);
-  const one = (change - (change % 1)) / 1
-  change = change - (1 * one);
-
-  return fiftyThousand + tenThousand + fiveThousand + oneThousand + fiveHundred + oneHundred + ten + five + one;
+  let change = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1];
+  let count = 0;
+  for (let i = 0; i < change.length; i++) {
+    while (change[i] <= A) {
+      A = A - change[i];
+      count++;
+      if (A === 0) break;
+    }
+  }
+  return count++;
 }
-
-console.log(solution(50000));
-console.log(solution(67000));
