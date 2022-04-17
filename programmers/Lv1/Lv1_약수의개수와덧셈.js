2a30c4a5477a97395677726a78df5e4cfb5ff94c
jinnkimm7 <jinnkimm7@gmail.com>
2022-04-17 22:31:54 +0900

    Create Lv1_약수의개수와덧셈.js

---
+++ programmers/Lv1/Lv1_약수의개수와덧셈.js
@@ -0,0 +1,14 @@
+function solution(left, right) {
+  let answer = 0;
+  for (let i = left; i <= right; i++) {
+    if (Number.isInteger(Math.sqrt(i))) {
+      answer -= i;
+    } else {
+      answer += i;
+    }
+  }
+  return answer;
+}
+
+console.log(solution(13, 17));
+console.log(solution(24, 27));
