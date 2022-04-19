4508eb34d9d2e0504f1e6934f1bb637d4732db08
jinnkimm7 <jinnkimm7@gmail.com>
2022-04-19 23:09:58 +0900

    Update Lv1_이상한문자만들기.js

--- programmers/Lv1/Lv1_이상한문자만들기.js
+++ programmers/Lv1/Lv1_이상한문자만들기.js
@@ -1,10 +1,8 @@
 function solution(s) {
-  let arr = s.split(' ');
-  let answer = [];
-  for (let i = 0; i < arr.length; i++) {
-    answer.push(arr[i].split('').map((cur, j) => j % 2 ? cur.toLowerCase() : cur.toUpperCase()).join(''))
-  }
-  return answer.join(' ');
+  return s.split(' ')
+    .map(e => e.split('')
+      .map((e, i) => i % 2 === 0 ? e.toUpperCase() : e.toLowerCase())
+      .join('')).join(' ');
 }
 
 console.log(solution("try hello world"));
