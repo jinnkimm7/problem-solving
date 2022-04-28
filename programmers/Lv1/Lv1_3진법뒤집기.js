030d5bbafddc0582190bd85857fab6c669719ebb
jinnkimm7 <jinnkimm7@gmail.com>
2022-04-28 15:23:49 +0900

    Update Lv1_3진법뒤집기.js

--- programmers/Lv1/Lv1_3진법뒤집기.js
+++ programmers/Lv1/Lv1_3진법뒤집기.js
@@ -1,6 +1,11 @@
+// function solution(n) {
+//   return parseInt(n.toString(3).split('').reverse().join(''), 3);
+// }
+
+//다른 풀이
+
 function solution(n) {
-  let reversed_N = n.toString(3).split('').reverse().join('')
-  return parseInt(reversed_N, 3)
+  return parseInt([...n.toString(3)].reverse().join(''), 3);
 }
 
 console.log(solution(45));
