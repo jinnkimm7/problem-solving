63406e753f17d6a9bc6f25193963b7d54cfc6cf5
jinnkimm7 <jinnkimm7@gmail.com>
2022-05-14 17:30:54 +0900

    Update Lv1_두정수사이의합.js
    새로운 풀이 추가

--- programmers/Lv1/Lv1_두정수사이의합.js
+++ programmers/Lv1/Lv1_두정수사이의합.js
@@ -1,17 +1,22 @@
+// function solution(a, b) {
+//   let sum = 0;
+//   let num = [];
+//   // a와 b가 같을 경우
+//   if (a === b) return a;
+//   // a와 b가 다른 경우
+//   else {
+//     num.push(a, b);
+//     num.sort((a, b) => (a - b));
+//     for (let i = num[0]; i < num[1] + 1; i++) {
+//       sum += i;
+//     }
+//     return sum;
+//   }
+// }
+
 function solution(a, b) {
-  let sum = 0;
-  let num = [];
-  // a와 b가 같을 경우
-  if (a === b) return a;
-  // a와 b가 다른 경우
-  else {
-    num.push(a, b);
-    num.sort((a, b) => (a - b));
-    for (let i = num[0]; i < num[1] + 1; i++) {
-      sum += i;
-    }
-    return sum;
-  }
+  a > b && ([a, b] = [b, a]);
+  return Array(b - a + 1).fill(a).map((v, i) => v + i).reduce((a, c) => a + c);
 }
 
 console.log(solution(3, 5));
