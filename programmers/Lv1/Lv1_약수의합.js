99291cc98c169f55530af2df939444e33f49ac78
jinnkimm7 <jinnkimm7@gmail.com>
2022-05-17 23:52:08 +0900

    Update Lv1_약수의합.js
    다른 풀이

--- programmers/Lv1/Lv1_약수의합.js
+++ programmers/Lv1/Lv1_약수의합.js
@@ -1,11 +1,17 @@
+// function solution(n) {
+//   let arr = [];
+//   for (let i = 1; i <= n; i++) {
+//     if (n % i === 0) arr.push(i);
+//   }
+//   let sum = arr.reduce((acc, cur) => acc + cur);
+//   return sum;
+// }
+
 function solution(n) {
-  let arr = [];
-  for (let i = 1; i <= n; i++) {
-    if (n % i === 0) arr.push(i);
-  }
-  let sum = arr.reduce((acc, cur) => acc + cur);
-  return sum;
+  return Array(n).fill().map((v, i) => i + 1)
+    .reduce((a, c) => n % c ? a : a + c, 0);
 }
+
 
 console.log(solution(12));
 console.log(solution(5));
