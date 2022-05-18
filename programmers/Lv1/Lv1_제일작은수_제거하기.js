a6e43086dd5ad92b191854a2693b49fe96b6a407
jinnkimm7 <jinnkimm7@gmail.com>
2022-05-18 15:30:19 +0900

    Update Lv1_제일작은수_제거하기.js
    다른풀이

--- programmers/Lv1/Lv1_제일작은수_제거하기.js
+++ programmers/Lv1/Lv1_제일작은수_제거하기.js
@@ -18,10 +18,16 @@
 
 
 // 다른 사람 풀이
-const solution = (arr) => {
-  arr.splice(arr.indexOf(Math.min(...arr)), 1);
-  return arr.length ? arr : [-1];
-};
+// const solution = (arr) => {
+//   arr.splice(arr.indexOf(Math.min(...arr)), 1);
+//   return arr.length ? arr : [-1];
+// };
+
+function solution(arr) {
+  const min = Math.min(...arr);
+  const r = arr.filter(v => v !== min);
+  return r.length ? r : [-1];
+}
 
 console.log(solution([4, 3, 2, 1]));
 console.log(solution([10]));
