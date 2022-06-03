e9d04fde8ce2e65ed5f6c310e5d1a42af5ea71ec
jinnkimm7 <jinnkimm7@gmail.com>
2022-06-03 21:19:57 +0900

    Update Lv1_핸드폰번호가리기.js

--- programmers/Lv1/Lv1_핸드폰번호가리기.js
+++ programmers/Lv1/Lv1_핸드폰번호가리기.js
@@ -1,17 +1,21 @@
 //내 풀이
-function solution(phone_number) {
-  const len = phone_number.length - 4;
-  return '*'.repeat(len) + phone_number.slice(len);
+// function solution(phone_number) {
+//   const len = phone_number.length - 4;
+//   return '*'.repeat(len) + phone_number.slice(len);
+// }
+
+// 새 풀이
+// function solution(phone_number) {
+//   const length = phone_number.length;
+//   return phone_number.split('').fill('*', 0, length - 4).join('');
+// }
+
+function solution(n) {
+  return [...n].fill('*', 0, n.length - 4).join('');
 }
 
 console.log(solution('12345678'));
 console.log(solution('0277788888'));
-
-// 다른 사람 풀이
-function hide_numbers(s) {
-  return s.replace(/\d(?=\d{4})/g, "*");
-}
-
-console.log("결과 : " + hide_numbers('01033334444'));
-
+console.log(solution('0102524333'));
+console.log(solution('01023529367'));
 
