7af549b2fa6f958e7c8297c10990b25c60635d7a
jinnkimm7 <jinnkimm7@gmail.com>
2022-06-03 21:55:34 +0900

    Update Lv1_하샤드수.js

--- programmers/Lv1/Lv1_하샤드수.js
+++ programmers/Lv1/Lv1_하샤드수.js
@@ -8,6 +8,18 @@ function solution(x) {
   return (x % sum === 0) ? true : false;
 }
 
+// 두 번째 내 풀이
+function solution(x) {
+  const sum = x.toString()
+    .split('')
+    .map(e => parseInt(e))
+    .reduce((a, b) => a + b);
+
+  return x % sum === 0 ? true : false;
+}
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+
 console.log(solution(10));
 console.log(solution(12));
 console.log(solution(11));
