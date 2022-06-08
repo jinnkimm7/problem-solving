7a3e1da2ddb51937c16f9c6e56f76363cbd24e77
jinnkimm7 <jinnkimm7@gmail.com>
2022-06-08 13:09:50 +0900

    Update Lv1_문자열내맘대로정렬.js

--- programmers/Lv1/Lv1_문자열내맘대로정렬.js
+++ programmers/Lv1/Lv1_문자열내맘대로정렬.js
@@ -1,13 +1,17 @@
+// function solution(strings, n) {
+//   return strings.sort((a, b) => {
+//     if (a[n] > b[n]) return 1;
+//     if (a[n] < b[n]) return -1;
+//     if (a[n] === b[n]) {
+//       if (a > b) return 1;
+//       if (a < b) return -1;
+//       return 0;
+//     }
+//   })
+// }
+
 function solution(strings, n) {
-  return strings.sort((a, b) => {
-    if (a[n] > b[n]) return 1;
-    if (a[n] < b[n]) return -1;
-    if (a[n] === b[n]) {
-      if (a > b) return 1;
-      if (a < b) return -1;
-      return 0;
-    }
-  })
+  return strings.sort((a, b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
 }
 
 console.log(solution(["sun", "bed", "car"], 1));
