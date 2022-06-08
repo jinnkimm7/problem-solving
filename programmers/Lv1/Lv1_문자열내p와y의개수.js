8b919c330fdbd3167dc397e7299b62203d073568
jinnkimm7 <jinnkimm7@gmail.com>
2022-06-08 12:47:45 +0900

    Update Lv1_문자열내p와y의개수.js

--- programmers/Lv1/Lv1_문자열내p와y의개수.js
+++ programmers/Lv1/Lv1_문자열내p와y의개수.js
@@ -1,20 +1,26 @@
 //내 풀이
-function solution(s) {
-  let str = s.toLowerCase();
-  let count = 0;
+// function solution(s) {
+//   let str = s.toLowerCase();
+//   let count = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === 'p') count++;
-    else if (str[i] === 'y') count--;
-  }
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === 'p') count++;
+//     else if (str[i] === 'y') count--;
+//   }
 
-  return count === 0 ? true : false;
-}
+//   return count === 0 ? true : false;
+// }
 
 //다른 사람 풀이
 // function solution(s) {
 //   return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
 // }
+
+function solution(s) {
+  const p = s.split('').filter(v => v === 'p' || v === 'P');
+  const y = s.split('').filter(v => v === 'y' || v === 'Y');
+  return p.length === y.length;
+}
 
 console.log(solution("pPoooyY")); //true
 console.log(solution("Pyy")); //false
