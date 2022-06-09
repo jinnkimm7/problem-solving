1432ba1c8a0d3f312313388661edec9940c3b15f
jinnkimm7 <jinnkimm7@gmail.com>
2022-06-09 20:01:20 +0900

    Update Lv1_부족한금액계산하기.js

--- programmers/Lv1/Lv1_부족한금액계산하기.js
+++ programmers/Lv1/Lv1_부족한금액계산하기.js
@@ -1,9 +1,17 @@
+// function solution(price, money, count) {
+//   let sum = 0;
+//   for (let i = 1; i <= count; i++) {
+//     sum += price * i;
+//   }
+//   return money > sum ? 0 : sum - money;
+// }
+
+// 나중에 나의 다른 풀이
 function solution(price, money, count) {
-  let sum = 0;
-  for (let i = 1; i <= count; i++) {
-    sum += price * i;
-  }
-  return money > sum ? 0 : sum - money;
+  const fee = Array(count).fill()
+    .map((v, i) => i + 1).map(v => v * price)
+    .reduce((a, b) => a + b);
+  return money >= fee ? 0 : fee - money;
 }
 
 console.log(solution(3, 20, 4));
