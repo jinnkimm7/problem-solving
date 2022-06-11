931ea720d5adebb663b0e4d9225d46be9dbb5170
jinnkimm7 <jinnkimm7@gmail.com>
2022-06-11 17:15:21 +0900

    Update Lv1_행렬의덧셈.js

--- programmers/Lv1/Lv1_행렬의덧셈.js
+++ programmers/Lv1/Lv1_행렬의덧셈.js
@@ -1,15 +1,18 @@
+// function solution(arr1, arr2) {
+//   var answer = [[]];
+//   for (var i = 0; i < arr1.length; i++) {
+//     answer[i] = [];
+//     for (var j = 0; j < arr1[i].length; j++) {
+//       answer[i].push(arr1[i][j] + arr2[i][j]);
+//     }
+//   }
+//   return answer;
+// }
+
+// 다시 풀어보기
 function solution(arr1, arr2) {
-  var answer = [[]];
-  for (var i = 0; i < arr1.length; i++) {
-    answer[i] = [];
-    for (var j = 0; j < arr1[i].length; j++) {
-      answer[i].push(arr1[i][j] + arr2[i][j]);
-    }
-  }
-  return answer;
+  return arr1.map((arr, i) => arr.map((v, j) => v + arr2[i][j]));
 }
 
 console.log(solution([[1, 2], [2, 3]], [[3, 4], [5, 6]]));
 
-
-// 다시 풀어보기
