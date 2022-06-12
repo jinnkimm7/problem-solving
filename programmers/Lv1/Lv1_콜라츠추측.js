6b45641a492bb577c0f31b8f7356972bc61534fd
jinnkimm7 <jinnkimm7@gmail.com>
2022-06-12 15:01:56 +0900

    Update Lv1_콜라츠추측.js

--- programmers/Lv1/Lv1_콜라츠추측.js
+++ programmers/Lv1/Lv1_콜라츠추측.js
@@ -43,6 +43,16 @@ function solution(n) {
   else return count;
 }
 
+function solution(num) {
+  let count = 0;
+  while (num !== 1 && count !== 500) {
+    num % 2 === 0 ? num = num /= 2 : num = (num * 3) + 1;
+    count++
+  }
+
+  return num === 1 ? count : -1;
+}
+
 console.log(solution(6));
 console.log(solution(16));
 console.log(solution(626331));
