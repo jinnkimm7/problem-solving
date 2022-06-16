5d002f3376589f46953f8b6b0e4bce9fd93d525e
jinnkimm7 <jinnkimm7@gmail.com>
2022-06-16 15:23:23 +0900

    Update 1.그리디.js
    그리디 문제1

--- algorithm/1.그리디.js
+++ algorithm/1.그리디.js
@@ -15,21 +15,45 @@
 */
 
 // 해답
-function solution(n) {
-  let count = 0;
-  const coins = [500, 100, 50, 10];
+// function solution(n) {
+//   let count = 0;
+//   const coins = [500, 100, 50, 10];
 
-  for (let i = 0; i < coins.length; i++) {
-    count += Math.floor(n / coins[i]); //동전의 개수 === 몫
-    n = n % coins[i]; // 남은 돈 === 나머지
-  }
+//   for (let i = 0; i < coins.length; i++) {
+//     count += Math.floor(n / coins[i]); //동전의 개수 === 몫
+//     n = n % coins[i]; // 남은 돈 === 나머지
+//   }
 
-  return count;
-}
+//   return count;
+// }
 
-console.log(solution(1260));
+// console.log(solution(1260));
 
 /**
 1. 화폐의 종류가 K라고 할 때, 시간 복잡도는 N(K)이다.
  */
 
+/**
+ * 문제 1
+
+어떠한 수 N이 1이 될 때까지 다음의 두 과정 중 하나를 반복적으로 선택하여 수행하려고 합니다. 단, 두번째 연산은 N이 K로 나누어 떨어질 때만 선택할 수 있습니다.
+
+N에서 1을 뺍니다.
+N을 K로 나눕니다.
+예를 들어 N이 17, K가 4라고 가정합시다. 이 때 1번의 과정을 한 번 수행하면 N은 16이 됩니다. 이후에 2번의 과정을 두번 수행하면 N은 1이 됩니다. 결과적으로 이 경우 전체 과정을 실행한 횟수는 3이 됩니다. 이는 N을 1로 만드는 최소 횟수입니다.
+
+N과 K가 주어질 때 N이 1이 될 때까지 1번 혹은 2번의 과정을 수행해야 하는 최소 횟수를 구하는 프로그램을 작성하세요.
+ */
+
+function solution(n, k) {
+  let count = 0;
+  while (n !== 1) {
+    n % k === 0 ? n = n / k : n = n - 1;
+    count++;
+  }
+
+  return count;
+}
+
+console.log(solution(17, 4));
+console.log(solution(25, 5));
