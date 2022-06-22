a686ba73cc20a047226201191db72fc4d946c986
jinnkimm7 <jinnkimm7@gmail.com>
2022-06-22 14:55:06 +0900

    파일 추가

---
+++ algorithm/4.정렬.js
@@ -0,0 +1,8 @@
+const input = prompt('N과 K를 공백으로 기준 입력').split(' ');
+
+const arrA = prompt('공백 기준으로 입력').split(' ').sort();
+const arrB = prompt('공백 기준으로 입력').split(' ').sort().reverse();
+
+for (let i = 0; i < input[1]; i++) {
+
+}
