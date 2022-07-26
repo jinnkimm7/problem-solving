769cb512777ceb920d18266b3050c9ec3d4603ad
jinnkimm7 <jinnkimm7@gmail.com>
2022-07-26 11:21:18 +0900

    Lv1_x만큼 간격이 있는 n개의 숫자

---
+++ java/algorithm/src/algorithm/X만큼간격이있는n개의숫자.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+import java.util.*;
+
+public class X만큼간격이있는n개의숫자 {
+	  public long[] solution(int x, int n) {
+	        long[] answer = new long[n];
+	        for(int i = 0; i < n; i++) {
+	        	answer[i] = (long)x * (i + 1);
+//	        	long으로 형변환이 필요하다.
+	        }
+	        return answer;
+	    }
+}
