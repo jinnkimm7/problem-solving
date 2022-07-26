3b18c340543700d58156c59b7c7471230ffe3133
jinnkimm7 <jinnkimm7@gmail.com>
2022-07-26 11:38:45 +0900

    Lv1_평균구하기

---
+++ java/algorithm/src/algorithm/Average.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+class Average {
+    public double solution(int[] arr) {        
+    	double sum = 0;
+    	
+    	for(int i = 0; i < arr.length; i++) {
+        	sum += arr[i];
+        }
+        
+        double answer = sum / arr.length;
+        
+        return answer;
+    }
+}
