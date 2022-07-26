aa65b696a75ebe605ba6297d49e95984e5da030d
jinnkimm7 <jinnkimm7@gmail.com>
2022-07-26 10:59:46 +0900

    Lv1. 직사각형 별찍기

---
+++ java/algorithm/src/algorithm/직사각형_별찍기.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import java.util.*;
+
+public class 직사각형_별찍기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
