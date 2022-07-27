0f38b2588ce2bdf2a07be6c79fe40ebde318d1c4
jinnkimm7 <jinnkimm7@gmail.com>
2022-07-27 12:20:34 +0900

    Lv1_짝수와 홀수

---
+++ java/algorithm/src/programmers/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class EvenOrOdd {
+
+    public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+	
+//    Test code
+	public static void main(String[] args) {
+		EvenOrOdd test = new EvenOrOdd();
+		System.out.println(test.solution(3));
+		System.out.println(test.solution(4));
+	}
+}
