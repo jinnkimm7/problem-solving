b43080f02157d70fc05271e36d45519e2b09122d
jinnkimm7 <jinnkimm7@gmail.com>
2022-07-30 10:09:01 +0900

    구현_예제2

---
+++ java/algorithm/src/algorithm/Implementation2.java
@@ -0,0 +1,29 @@
+package algorithm;
+
+import java.util.*;
+
+public class Implementation2 {
+//	특정한 시각에 3이 포함되어 있는지의 여부
+	public static boolean check(int h, int m, int s) {
+		if(h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		h입력받기
+		int h = sc.nextInt();
+		int count = 0;
+		for(int i = 0; i <= h; i++) {
+			for (int j = 0; j < 60; j++) {
+				for (int k =0; k < 60; k++) {
+//					매 시각 안에 3이 포함되어 있다면 카운트 증
+					if(check(i,j,k)) count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
