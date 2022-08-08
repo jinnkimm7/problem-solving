f14b2f972593757cc854fe6347e0ea5defc537a0
jinnkimm7 <jinnkimm7@gmail.com>
2022-08-08 12:28:45 +0900

    Lv1_약수의 합

---
+++ java/algorithm/src/programmers_Lv1/SumDivisor.java
@@ -0,0 +1,32 @@
+package programmers_Lv1;
+
+public class Solution {
+    public int solution(int n) {
+//        int answer = 0;
+//        
+//        int[] intArr = new int[n];
+//        for(int i = 0; i < n; i++) {
+//        	intArr[i] = i + 1;
+//        }
+//        
+//        for(int i = 0; i < intArr.length; i++) {
+//        	if(n % intArr[i] == 0) answer += intArr[i];
+//        }
+//        
+//        return answer;
+    	int answer = 0;
+    	
+    	for(int i = 1; i <= n; i++) {
+    		if(n % i == 0) answer += i;
+    	}
+    	
+    	return answer;
+    }
+    
+//    Test code
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.solution(12));
+		System.out.println(s.solution(5));
+	}
+}
