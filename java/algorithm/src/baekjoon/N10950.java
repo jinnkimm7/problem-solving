9cbfd111003d6d2e27c8637cb70b3ffe71e4ed55
jinnkimm7 <jinnkimm7@gmail.com>
2022-08-15 14:42:09 +0900

    Create N10950.java

---
+++ java/algorithm/src/baekjoon/N10950.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.*;
+
+public class N10950 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+//		sc.nextLine();
+		
+		for(int i = 0; i < t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a+b);
+		}
+	}
+}
