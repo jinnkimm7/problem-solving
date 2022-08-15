9c677f91766287d4aaf8d570fd33f00230528d7c
jinnkimm7 <jinnkimm7@gmail.com>
2022-08-15 14:48:23 +0900

    Create N8393.java

---
+++ java/algorithm/src/baekjoon/N8393.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.*;
+
+public class N8393 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int answer = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			answer += i;
+		}
+		
+		System.out.println(answer);
+	}
+}
