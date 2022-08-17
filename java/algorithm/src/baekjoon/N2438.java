d757139c160fcfa66fa8c5ac78af3cc1268d136d
jinnkimm7 <jinnkimm7@gmail.com>
2022-08-17 16:37:29 +0900

    Create N2438.java

---
+++ java/algorithm/src/baekjoon/N2438.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.*;
+
+public class N2438 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+//		첫번째 내 풀이 : repeat 메서드 사용
+//		int n = sc.nextInt();
+//		
+//		for(int i = 1; i <= n; i++) {
+//			System.out.println("*".repeat(i));
+//		}
+		
+//		두번째 풀이
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			sout
+		}
+	}
+}
