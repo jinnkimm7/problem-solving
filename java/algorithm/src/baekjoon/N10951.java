0015126ea7e378894b41b4ddcee71ed076c667ae
jinnkimm7 <jinnkimm7@gmail.com>
2022-08-18 10:22:10 +0900

    Create N10951.java

---
+++ java/algorithm/src/baekjoon/N10951.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.*;
+
+public class N10951 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			System.out.println(A + B);
+		}
+	}
+}
