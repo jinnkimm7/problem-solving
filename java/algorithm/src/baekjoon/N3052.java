bb07c80477e715976971e88fce1801ab727db80c
jinnkimm7 <jinnkimm7@gmail.com>
2022-08-19 15:26:53 +0900

    Update N3052.java

--- java/algorithm/src/baekjoon/N3052.java
+++ java/algorithm/src/baekjoon/N3052.java
@@ -5,15 +5,15 @@ import java.util.*;
 public class N3052 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] nums = new int[10];
+//		int[] nums = new int[10];
 		Set<Integer> set = new HashSet<>();
 		
-		for(int i = 0; i < 10; i++) {
-			nums[i] = sc.nextInt();
-		}
+//		for(int i = 0; i < 10; i++) {
+//			nums[i] = sc.nextInt();
+//		}
 		
 		for(int i = 0; i < 10; i++) {
-			set.add(nums[i] % 42); 
+			set.add(sc.nextInt() % 42); 
 		}
 		
 		System.out.println(set.size());		
