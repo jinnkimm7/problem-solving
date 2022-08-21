0caf87bbd557f43660d178b1a652f9d99e71f2d0
jinnkimm7 <jinnkimm7@gmail.com>
2022-08-21 23:44:10 +0900

    Create N11654.java

---
+++ java/algorithm/src/baekjoon/N11654.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.*;
+
+public class N11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int result = sc.next().charAt(0);
+		
+		System.out.println(result);
+	}
+}
