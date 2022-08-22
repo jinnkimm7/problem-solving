cb4c14eb0b2266b3392ee60eb8071ed2bea5b671
jinnkimm7 <jinnkimm7@gmail.com>
2022-08-22 11:11:53 +0900

    N10809
    나중에 다시 풀어보기

---
+++ java/algorithm/src/baekjoon/Main.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] arr = new int[26];
+		for(int i = 0; i < arr.length; i ++) {
+			arr[i] = -1;
+		}
+		
+		String S = sc.next();
+		
+		for(int i = 0; i < S.length(); i++) {
+			char ch = S.charAt(i);
+			
+			if(arr[ch - 'a'] == -1) {
+				arr[ch - 'a'] = i;
+			}
+		}
+
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
