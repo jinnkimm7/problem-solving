5cc1ab698ff1402af1d058305ef63888e044d46c
jinnkimm7 <jinnkimm7@gmail.com>
2022-08-24 11:30:30 +0900

    Create N1152.java

---
+++ java/algorithm/src/baekjoon/N1152.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.*;
+
+public class N1152 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine().trim();
+		
+		String[] arr = s.split(" ");
+		
+		if(s.isEmpty()) System.out.println(0);
+		else System.out.println(arr.length);
+	}
+}
