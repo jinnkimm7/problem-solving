7b4c23b20a8f17a3fe46fe3bd8697434c68ad12b
jinnkimm7 <jinnkimm7@gmail.com>
2022-08-25 11:29:34 +0900

    Create N2750.java

---
+++ java/algorithm/src/baekjoon/N2750.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.*;
+
+public class N2750 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
