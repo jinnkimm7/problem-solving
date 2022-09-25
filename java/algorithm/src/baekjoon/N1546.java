15146312f37ddfaf70960dfab0dc8889d7dc229e
jinnkimm7 <jinnkimm7@gmail.com>
2022-09-25 19:54:52 +0900

    Create N1546.java

---
+++ java/algorithm/src/baekjoon/N1546.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.*;
+
+public class N1546 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		double[] arr = new double[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextDouble();
+		}
+		
+		Arrays.sort(arr);
+		double M = arr[arr.length - 1];
+		
+		double sum = 0;
+		for(int i = 0; i < N; i++) {
+			sum += (arr[i] / M * 100);
+		}
+		
+		System.out.println(sum / N);
+	}
+}
