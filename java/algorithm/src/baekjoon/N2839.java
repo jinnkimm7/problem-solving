9fa50204dd910a4f8d3ed6c69601db1ce1b450f3
jinnkimm7 <jinnkimm7@gmail.com>
2022-10-12 18:11:50 +0900

    Create N2839.java
    다시 풀기

---
+++ java/algorithm/src/baekjoon/N2839.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.*;
+
+public class N2839 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); // 배달해야 되는 N kg
+		
+		if(N == 4 || N == 7) {
+			System.out.println(-1);
+		} else if (N % 5 == 0) {
+			System.out.println(N / 5);
+		} else if ((N % 5 == 1) || (N % 5 == 3)) {
+			System.out.println((N / 5) + 1);
+		} else if ((N % 5 == 2) || (N % 5 == 4)) {
+			System.out.println((N / 5) + 2);
+		} 
+	}
+}
