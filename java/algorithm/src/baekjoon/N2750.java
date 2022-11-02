1e25094480b03eb292449418474fddfbc0f94b70
jinnkimm7 <jinnkimm7@gmail.com>
2022-11-02 13:46:39 +0900

    Update N2750.java
    정렬 알고리즘 사용 (선택 정렬)

--- java/algorithm/src/baekjoon/N2750.java
+++ java/algorithm/src/baekjoon/N2750.java
@@ -12,7 +12,22 @@ public class N2750 {
 		for(int i = 0; i < N; i++) {
 			arr[i] = sc.nextInt();
 		}
-		Arrays.sort(arr);
+		
+		// Arrays.sort(arr);
+		// 정렬 알고리즘
+		for(int i = 0; i < N; i++) {
+			int min_index = i;
+			
+			for(int j = i + 1; j < N; j++) {
+				if(arr[min_index] > arr[j]) {
+					min_index = j;
+				}
+			}
+			
+			int temp = arr[i];
+			arr[i] = arr[min_index];
+			arr[min_index] = temp;
+		}
 		
 		for(int i = 0; i < N; i++) {
 			System.out.println(arr[i]);
