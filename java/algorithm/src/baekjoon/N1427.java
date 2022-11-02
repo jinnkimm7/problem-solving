28039e2f687091193e386cbaef1b60d423956c86
jinnkimm7 <jinnkimm7@gmail.com>
2022-11-02 23:20:56 +0900

    Update N1427.java
    counting 정렬로 풀기

--- java/algorithm/src/baekjoon/N1427.java
+++ java/algorithm/src/baekjoon/N1427.java
@@ -13,5 +13,28 @@ public class N1427 {
 		for(int i = arr.length - 1; i >= 0; i--) {
 			System.out.print(arr[i]);
 		}
+		
+		// counting 정렬로 풀어보기
+//		Scanner sc = new Scanner(System.in);
+//		StringBuilder sb = new StringBuilder();
+//		
+//		int N = sc.nextInt();
+//		
+//		int[] counting = new int[10]; 
+//		
+//		while(N != 0) {
+//			counting[N % 10] += 1;
+//			N /= 10;
+//		}
+//	
+////		System.out.println(Arrays.toString(counting));
+//		
+//		for(int i = 9; i >= 0; i--) {
+//			for(int j = 0; j < counting[i]; j++) {
+//				sb.append(i);
+//			}
+//		}
+//		System.out.println(sb);
+	}
 	}
 }
