97515b73a8d691a176cd3a30172f37b0093319e9
jinnkimm7 <jinnkimm7@gmail.com>
2022-11-03 21:35:20 +0900

    Update N18870.java

--- java/algorithm/src/baekjoon/N18870.java
+++ java/algorithm/src/baekjoon/N18870.java
@@ -6,39 +6,32 @@ public class N18870 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt(); // 좌표의 개수
-		int[] X = new int[N]; // X좌표
-		for(int i = 0; i < N; i++) {
-			X[i] = sc.nextInt();
+		int N = sc.nextInt();
+		
+		int[] origin = new int[N]; //원래 배열
+		int[] sorted = new int[N]; //정렬할 배열
+		Map<Integer, Integer> rankingMap = new HashMap<>();
+		
+ 		for(int i = 0; i < N; i++) {
+			origin[i] = sorted[i] = sc.nextInt();
 		}
 		
-		Set<Integer> set = new HashSet<>();
-		for(int i = 0; i < N; i++) {
-			set.add(X[i]);
-		}
+ 		Arrays.sort(sorted);
 		
-		int[] location = new int[set.size()]; // 실제 X좌표(중복제거)
-		int k = 0;
-		for(int i : set) {
-			location[k++] = i;
-		}
-
-//		System.out.println(Arrays.toString(X));
-//		System.out.println(Arrays.toString(location));
-		
-		int[] answer = new int[N]; // 압축된 좌표
-		
-		for(int i = 0; i < N; i++) {
-			int count = 0;
-			for(int j = 0; j < location.length; j++) {
-				if(X[i] > location[j]) count++;
-			}
-			answer[i] = count;
-		}
-		
-//		System.out.println(Arrays.toString(answer));
-		for(int i = 0; i < answer.length; i++) {
-			System.out.print(answer[i] + " ");
-		}
+ 		int rank = 0;
+ 		for(int v : sorted) {
+ 			if(!rankingMap.containsKey(v)) {
+ 				rankingMap.put(v, rank);
+ 				rank++;
+ 			}
+ 		}
+ 		
+ 		StringBuilder sb = new StringBuilder();
+ 		
+ 		for(int i = 0; i < N; i++) {
+ 			sb.append(rankingMap.get(origin[i])).append(" ");
+ 		}
+ 		
+ 		System.out.println(sb);
 	}
 }
